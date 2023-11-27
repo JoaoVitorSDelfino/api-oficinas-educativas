9ebd2a902a4bd349ab615b45cd09c989e94f45e5
João Vitor Schiavinato Delfino <joao.delfino.pb@compasso.com.br>
2023-11-27 17:58:40 -0300

    feat: conexão com o banco de dados

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ const express = require("express")
 const app = express()
 const jwt = require("jsonwebtoken")
 
+// Conexão com o banco de dados
+const db = require('./db/connection')
+
+db.authenticate().then(() => {
+    console.log("Conectou ao banco de dados com sucesso");
+}).catch(err => {
+    console.log("Ocorreu um erro ao conectar", err);
+})
+
 app.get('/', (req, res) => {
     res.send('teste')
 });
