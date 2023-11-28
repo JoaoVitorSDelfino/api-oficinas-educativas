9adb9b9955dcf47fc3bc86877cb767c476465b4f
João Vitor Schiavinato Delfino <joao.delfino.pb@compasso.com.br>
2023-11-28 11:24:06 -0300

    feat: método GET para listar todas as oificinas

--- src/routes/oficinas.js
+++ src/routes/oficinas.js
@@ -2,6 +2,15 @@ const express = require('express')
 const router = express.Router()
 const Oficina = require("../models/oficina")
 
+// Listar todas as oficinas cadastradas
+router.get('/', async (req, res) => {
+    const oficinas = await Oficina.findAll();
+    const jsonOficinas = JSON.stringify({ lista: oficinas }, null, 2);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(jsonOficinas);
+})
+
 // Adicionar nova oficina
 router.post('/add', async (req, res) => {
     try {
