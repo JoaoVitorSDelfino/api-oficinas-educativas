dffc51fbab1aa27a856fc451c7ff6f07d64c957e
João Vitor Schiavinato Delfino <joao.delfino.pb@compasso.com.br>
2023-11-28 17:17:13 -0300

    fix: validar data de oficinas

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -8,15 +8,30 @@ const validarCampoString = (valor, mensagemErro) => {
 };
 
 const validarData = (data) => {
-    const padraoData = /^\d{2}-\d{2}-\d{4}$/;
+    // Define padrão DD-MM-AAAA
+    const padraoData = /^\d{2}-\d{2}-\d{4}$/
 
     if (data == '' || data == null) {
-        return {status: false, mensagem: 'ERRO, data não pode ser vazia!'};
+        return {status: false, mensagem: 'ERRO, data não pode ser vazia!'}
     } else if (!padraoData.test(data)) {
-        return {status: false, mensagem: 'Formato de data inválido. Use o formato DD-MM-AAAA.'};
+        return {status: false, mensagem: 'ERRO, formato de data inválido! Use o formato DD-MM-AAAA.'}
+    } else {
+        let [dia, mes, ano] = data.split('-')
+
+        dia = parseInt(dia, 10)
+        mes = parseInt(mes, 10)
+        ano = parseInt(ano, 10)
+
+        // Verifica se dia ou mês digitados são válidos
+        // Valida também para anos bissextos
+        if (dia > 31 || (dia > 28 && mes == 2 && (ano % 4 != 0)) ||(dia > 29 && mes == 2 && (ano % 4 == 0))) {
+            return {status: false, mensagem: 'ERRO, o dia informado é inválido!'}
+        } else if (mes < 1 || mes > 12) {
+            return {status: false, mensagem: 'ERRO, o mês informado é inválido!'}
+        }
     }
 
-    return {status: true, mensagem: ''};
+    return {status: true, mensagem: ''}
 };
 
 module.exports = {
