0befdafd7cf28a46163dc71c83dee41fff628634
João Vitor Schiavinato Delfino <joao.delfino.pb@compasso.com.br>
2023-11-29 09:01:45 -0300

    feat: método GET para listar todos os usuários

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Usuario = require("../models/usuario");
+const validation = require('../utils/validation')
+
+// Listar todos os usuários cadastrados
+router.get('/', async (req, res) => {
+    const usuarios = await Usuario.findAll();
+    const jsonUsuarios = JSON.stringify({ lista: usuarios }, null, 2);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(jsonUsuarios);
+})
 
 module.exports = router;
