a6a5b9800d2dac7921040fb04ca9f6d6ef39addd
João Vitor Schiavinato Delfino <joao.delfino.pb@compasso.com.br>
2023-11-29 19:48:49 -0300

    feat: método PUT para editar um organizador

--- src/routes/organizadores.js
+++ src/routes/organizadores.js
@@ -74,4 +74,47 @@ router.get('/view/:id', async (req, res) => {
     }
 })
 
+// Alterar um organizador pelo id
+router.put('/edit/:id', async (req, res) => {
+    try {
+        organizador = await Organizador.findOne({
+            where: {id: req.params.id}
+        })
+
+        // Valida se valores digitados são válidos
+        if (validation.validarOrganizador(req.body).status) {
+            const {idUsuario, idOficina} = req.body 
+
+            const organizadorExiste = await Organizador.findOne({
+                where: {
+                    idUsuario,
+                    idOficina
+                },
+            })
+    
+            // Verifica se organizador que deseja criar já existe
+            if (!organizadorExiste) {
+                await Organizador.update(
+                    req.body, 
+                    {where: {id: req.params.id}}
+                )
+
+                organizadorAtualizada = await Oficina.findOne({
+                    where: {id: req.params.id}
+                })
+
+                res.json({status: 'Oficina alterada com sucesso!', organizadorAtualizada})
+            } else {
+                console.log('ERRO, organizador já existe.')
+                res.status(400).json({ error: 'ERRO, organizador já existe.' })
+            }
+        } else {
+            res.status(500).json({error: 'ERRO, organizador não existe!'})
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({error: 'ERRO ao editar oficina.'})
+    }
+})
+
 module.exports = router;
